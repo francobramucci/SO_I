1f24875c270401ad37408fc6c4760e7c8a331699
francobramucci <bramuccifranco1@gmail.com>
2026-06-03 00:28:05 -0300

    my_cond_bak.c versión no terminada. Propongo como solución my_cond.h

--- P3/13/my_cond_bak.c
+++ P3/13/my_cond_bak.c
@@ -4,10 +4,11 @@
 void my_cond_init(my_cond_t *cond) {
     // Inicializa el semáforo en 0, cuando se haga wait quedará lockeado.
     sem_init(&cond->sem, 0, 0);
-    sem_init(&cond->sem1, 0, 1);
+    sem_init(&cond->sem1, 0, 0);
     sem_init(&cond->sem2, 0, 1);
     cond->n = 0; // Nodos que tienen que despertar
     cond->d = 0; // Nodos esperando despertar
+    cond->d = 0; // Nodos en sala de espera
 }
 
 void my_cond_wait(my_cond_t *cond, pthread_mutex_t *mutex) {
@@ -18,19 +19,26 @@ void my_cond_wait(my_cond_t *cond, pthread_mutex_t *mutex) {
     // suelta el mutex.
     pthread_mutex_unlock(mutex);
 
-    sem_post(&cond->sem2); // unlock
+    if (cond->n > 0) {
+        cond->s++;
+        sem_post(&cond->sem2); // unlock
+
+        sem_wait(&cond->sem1);
+        sem_post(&cond->sem1);
+    } else {
+        sem_post(&cond->sem2); // unlock
+    }
 
     // Baja el semáforo y espera un signal o un broadcast.
     sem_wait(&cond->sem);
 
-    sem_wait(&cond->sem1);
+    sem_wait(&cond->sem2);
     cond->n--;
     cond->d--;
     if (cond->n == 0) {
-        sem_post(&cond->sem2); // unlock
+        sem_post(&cond->sem1);
     }
-
-    sem_post(&cond->sem1);
+    sem_post(&cond->sem2);
 
     // pide el mutex.
     pthread_mutex_lock(mutex);
@@ -39,13 +47,16 @@ void my_cond_wait(my_cond_t *cond, pthread_mutex_t *mutex) {
 void my_cond_signal(my_cond_t *cond) {
     sem_wait(&cond->sem2); // lock
 
+    if (cond->s != cond->n)
+        sem_init(&cond->sem1, 0, 0);
+
     if (cond->d > 0) {
         cond->n++;
         // Levanta el semáforo.
         sem_post(&(cond->sem));
-    } else {
-        sem_post(&cond->sem2);
     }
+
+    sem_post(&cond->sem2);
 }
 
 void my_cond_broadcast(my_cond_t *cond) {
@@ -56,12 +67,12 @@ void my_cond_broadcast(my_cond_t *cond) {
         cond->n++;
         ndesp--;
     }
+    sem_post(&cond->sem2);
 }
 
 void my_cond_destroy(my_cond_t *cond) {
     // Destruye el semáforo.
-    sem_destroy(&cond->sem);
-    sem_destroy(&cond->sem1);
+    sem_destroy(&(cond->sem));
     sem_destroy(&cond->sem2);
 }
 
